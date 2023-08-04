1a2a926ec01fb527e799680a2f6ff50b0016d3c5
jayakumar03 <jaimech61@gmail.com>
2023-08-04 10:24:55 +0530

    Added middle ware for cookies

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken")
+ 
+const auth = (req, res, next) => {
+    const token = 
+        req.cookies.token ||
+        res.body.token ||
+        req.header("Authorization").replace("Bearer ", "")  
+    
+    if (!token) {
+         return res.status(403).send("token is missing")
+    }
+
+    try {
+        const decode = jwt.verify(token, process.env.SECRET_KEY)
+        console.log(decode)
+        req.user = decode
+        
+    } catch (error) {
+        return res.status(401).send("Invalid Token", error)
+        
+    }
+
+    return next()
+}
+
+module.exports = auth;
